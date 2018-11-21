d88ab9bcaf1163fbc98107d5a6a439014b255852
EoinDuffill <eoincduffill@btinternet.com>
2018-11-21 15:28:26 +0000

    Script/node to read in a .gpx file (GPS points), and publish the points in the interval they were recorded initially in

---
+++ src/gps_point_publisher.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+import sys
+import rospy
+import time
+import xml.etree.ElementTree as ET
+from std_msgs.msg import Float32, Float32MultiArray, MultiArrayDimension
+
+class automower_gps(object):
+
+	def init(self):
+		#Setup experiment data publisher
+ 	   	self.pub_gps_data = rospy.Publisher('slam/gps_receiver', Float32MultiArray, queue_size=1)
+		#initialise list variables
+		self.lats = []
+		self.longs = []
+		self.times = []
+
+	def parse_points(self):
+		#open gps points file
+		tree = ET.parse("test_points.gpx")
+		#Get root of xml tree
+		root = tree.getroot()
+		#start time of the gps points var
+		start_time = None
+		#Iterate over each gps point
+		for elem in root.iter("{http://www.topografix.com/GPX/1/0}trkpt"):
+			#Find sub elements that have the source GPS get time
+			for sub_elem1 in elem.iter("{http://www.topografix.com/GPX/1/0}src"):
+				for sub_elem2 in elem.iter("{http://www.topografix.com/GPX/1/0}time"):
+					if sub_elem1.text == "gps":
+						print sub_elem1.text
+						#transform to seconds
+						time = self.get_seconds(sub_elem2.text.split("Z")[0].split("T")[1])
+						#Initialise start time var if not done so already					
+						if start_time == None:
+							start_time = time
+						time = time - start_time
+						#append lats,longs and times to the lists of each
+						self.lats.append(float(elem.attrib['lat']))
+						self.longs.append(float(elem.attrib['lon']))
+						self.times.append(time)
+			
+	#transform HH:MM:SS.mm into seconds
+	def get_seconds(self, time):
+		h, m, s = time.split(":")
+		return int(h) * 3600 + int(m) * 60 + float(s)
+
+	def publish_all_gps_points(self):
+		prev_time = None
+		time_diff = 0
+		#Iterate over each lat,long and time
+		for i in range(len(self.times)):
+			print(self.lats[i], self.longs[i], self.times[i])
+			if prev_time != None:
+				#difference in time between previous gps point and current
+				time_diff = self.times[i] - prev_time
+				print(self.times[i] - prev_time)
+				if time_diff > 0:
+					#Sleep thread for the time difference between last point and current
+					#Then publish the GPS point
+					time.sleep(self.times[i] - prev_time)
+					self.publish_gps_point(self.lats[i], self.longs[i], self.times[i])
+			if(time_diff >= 0):
+				#Aslong as positive time_diff set new previous points time
+				prev_time = self.times[i]
+
+	def publish_gps_point(self, lat, lon, time):
+		#Publish current experiment data for mobile device to subscribe to. 
+		mat = Float32MultiArray()
+		#set up data structure
+		mat.layout.dim.append(MultiArrayDimension())
+		mat.layout.dim[0].label = "gps_data"
+		mat.layout.dim[0].size = 3
+		mat.layout.dim[0].stride = 1
+		mat.layout.data_offset = 0
+		mat.data = [0]*3
+		#lat, lon, and time
+		mat.data[0] = lat
+		mat.data[1] = lon
+		mat.data[2] = time
+		#publish result
+		self.pub_gps_data.publish(mat)
+
+	def fini(self):
+		print("finishing")
+
+	def run(self):
+		try:
+			self.init()
+			self.parse_points()
+			self.publish_all_gps_points()
+		except rospy.exceptions.ROSInterruptException:
+			pass
+		finally:
+			self.fini()
+
+if __name__ == '__main__':
+	rospy.init_node('automower_gps_publisher')
+	slam_gps = automower_gps()
+	slam_gps.run()
