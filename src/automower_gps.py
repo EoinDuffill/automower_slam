2d8822667d61bc71a79265b91c785ba091fcfe04
EoinDuffill <eoincduffill@btinternet.com>
2018-11-21 15:30:01 +0000

    Script/node to subscribe to publish GPS points, convert them to x,y co-ordinates (and currently plot the results)

---
+++ src/automower_gps.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import Image
+import pyproj as proj
+from std_msgs.msg import Float32MultiArray
+
+class automower_gps(object):
+
+	def init(self):
+		#Update rate in Hz
+		self.update_rate = 10
+
+		#Initialising vars
+		self.lats = []
+		self.longs = []
+		self.time = 0
+		#variable to compare against gps point's time
+		self.plot_time = 0
+		#Time between plots
+		self.plot_interval = 10
+
+	def receive_gps_point(self, data):
+		#Recieved gps point, append it to lists
+		print ("Point received...")
+		self.lats.append(data.data[0])
+		self.longs.append(data.data[1])
+		self.time = data.data[2]
+		print data.data[0], data.data[1], data.data[2]
+
+	def update(self):
+		if(self.time > self.plot_time and self.time != None):
+			#If it has been plot_interval seconds since last plot then plot the data
+			self.plot_data()
+			self.plot_time = self.time + self.plot_interval
+
+	def plot_data(self):
+		print("plotting")
+		
+		#Projection vars for the UK
+		crs_wgs = proj.Proj(init='epsg:4326')
+		crs_bng = proj.Proj(init='epsg:27700')
+		
+		#Function to transform latitudes and longitudes to localised x,y co-ordinates
+		x,y = proj.transform(crs_wgs, crs_bng, np.array(self.longs), np.array(self.lats))
+
+		#Plot parsed points
+		plt.plot(x, y)
+		#axis sqaured off, uniform in x and y
+		plt.axis('equal')
+		#Set axis based on min and max vals in x and y
+		plt.axis([np.amin(x) - 1, np.amax(x) + 1, np.amin(y)  - 1, np.amax(y) + 1])
+		#Save figure to file
+		print("Saving Plot...")
+		plt.savefig('gps_plot.png')	
+	
+	def fini(self):
+		print('Finishing...')
+
+	def run(self):
+		try:
+			self.init()
+			#Setup subscribers
+			print ("Setting up subscriber...")
+			rospy.Subscriber('slam/gps_receiver', Float32MultiArray, self.receive_gps_point, queue_size=1)
+			r = rospy.Rate(self.update_rate)
+			while not rospy.is_shutdown():
+				self.update()
+				r.sleep()
+		except rospy.exceptions.ROSInterruptException:
+			pass
+		finally:
+			self.fini()
+
+
+if __name__ == '__main__':
+	rospy.init_node('automower_slam_gps')
+	automower_gps = automower_gps()
+	automower_gps.run()
