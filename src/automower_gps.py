f47afed3d43658af434caa80df4f7dc25d2ad2bc
EoinDuffill <eoincduffill@btinternet.com>
2019-04-12 12:47:38 +0100

    misc GPS changes

--- src/automower_gps.py
+++ src/automower_gps.py
@@ -1,67 +1,70 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-import math
 import matplotlib.pyplot as plt
-import Image
 import pyproj as proj
 from std_msgs.msg import Float32MultiArray
 
-class automower_gps(object):
 
-	def init(self):
-		#Update rate in Hz
+class AutomowerGps(object):
+
+	def __init__(self):
+		# Update rate in Hz
 		self.update_rate = 10
 
-		#Initialising vars
+		# open file
+		self.file = open("results_gps.txt", "w")
+
+		# Initialising vars
 		self.lats = []
 		self.longs = []
 		self.time = 0
-		#variable to compare against gps point's time
+		# variable to compare against gps point's time
 		self.plot_time = 0
-		#Time between plots
+		# Time between plots
 		self.plot_interval = 10
 
 	def receive_gps_point(self, data):
-		#Recieved gps point, append it to lists
-		print ("Point received...")
+		# Recieved gps point, append it to lists
+		print("Point received...")
 		self.lats.append(data.data[0])
 		self.longs.append(data.data[1])
 		self.time = data.data[2]
-		print data.data[0], data.data[1], data.data[2]
+		self.file.write(str(data.data[0])+", "+str(data.data[1])+"\n")
+		print(data.data[0], data.data[1], data.data[2])
 
 	def update(self):
-		if(self.time > self.plot_time and self.time != None):
-			#If it has been plot_interval seconds since last plot then plot the data
+		if self.time > self.plot_time and self.time is not None:
+			# If it has been plot_interval seconds since last plot then plot the data
 			self.plot_data()
 			self.plot_time = self.time + self.plot_interval
 
 	def plot_data(self):
 		print("plotting")
 		
-		#Projection vars for the UK
+		# Projection vars for the UK
 		crs_wgs = proj.Proj(init='epsg:4326')
 		crs_bng = proj.Proj(init='epsg:27700')
 		
-		#Function to transform latitudes and longitudes to localised x,y co-ordinates
+		# Function to transform latitudes and longitudes to localised x,y co-ordinates
 		x,y = proj.transform(crs_wgs, crs_bng, np.array(self.longs), np.array(self.lats))
 
-		#Plot parsed points
+		# Plot parsed points
 		plt.plot(x, y)
-		#axis sqaured off, uniform in x and y
+		# axis squared off, uniform in x and y
 		plt.axis('equal')
-		#Set axis based on min and max vals in x and y
+		# Set axis based on min and max vals in x and y
 		plt.axis([np.amin(x) - 1, np.amax(x) + 1, np.amin(y)  - 1, np.amax(y) + 1])
-		#Save figure to file
+		# Save figure to file
 		print("Saving Plot...")
 		plt.savefig('gps_plot.png')	
-	
-	def fini(self):
+
+	@staticmethod
+	def fini():
 		print('Finishing...')
 
 	def run(self):
 		try:
-			self.init()
 			#Setup subscribers
 			print ("Setting up subscriber...")
 			rospy.Subscriber('slam/gps_receiver', Float32MultiArray, self.receive_gps_point, queue_size=1)
@@ -77,5 +80,5 @@ class automower_gps(object):
 
 if __name__ == '__main__':
 	rospy.init_node('automower_slam_gps')
-	automower_gps = automower_gps()
+	automower_gps = AutomowerGps()
 	automower_gps.run()
--- src/gps_point_publisher.py
+++ src/gps_point_publisher.py
@@ -1,54 +1,54 @@
 #!/usr/bin/env python
-import sys
+import sys, tty, select
 import rospy
 import time
 import xml.etree.ElementTree as ET
 import json
 from std_msgs.msg import Float32, Float32MultiArray, MultiArrayDimension
 
-class automower_gps(object):
+
+class AutomowerGpsPublisher(object):
 
 	def __init__(self, source):
-		#GPS Source, 0 is live, 1 is from .gpx file#
-		#live data from gpspipe: gpspipe -w | ./<file_name>.py 1
+		# GPS Source, 0 is live, 1 is from .gpx file#
+		# live data from gpspipe: gpspipe -w | ./<file_name>.py 1
 		self.gps_receiver = source
 
-	def init(self):
-		#Setup experiment data publisher
- 	   	self.pub_gps_data = rospy.Publisher('slam/gps_receiver', Float32MultiArray, queue_size=1)
-		#initialise list variables
+		# Setup experiment data publisher
+		self.pub_gps_data = rospy.Publisher('slam/gps_receiver', Float32MultiArray, queue_size=1)
+		# initialise list variables
 		self.lats = []
 		self.longs = []
 		self.times = []
 
-	#parse GPS xml file
+	# parse GPS xml file
 	def parse_points_file(self):
-		#open gps points file
+		# open gps points file
 		tree = ET.parse("test_points.gpx")
-		#Get root of xml tree
+		# Get root of xml tree
 		root = tree.getroot()
-		#start time of the gps points var
+		# start time of the gps points var
 		start_time = None
-		#Iterate over each gps point
+		# Iterate over each gps point
 		for elem in root.iter("{http://www.topografix.com/GPX/1/0}trkpt"):
-			#Find sub elements that have the source GPS get time
+			# Find sub elements that have the source GPS get time
 			for sub_elem1 in elem.iter("{http://www.topografix.com/GPX/1/0}src"):
 				for sub_elem2 in elem.iter("{http://www.topografix.com/GPX/1/0}time"):
 					if sub_elem1.text == "gps":
-						print sub_elem1.text
-						#transform to seconds
+						# transform to seconds
 						time = self.get_seconds(sub_elem2.text)
-						#Initialise start time var if not done so already					
-						if start_time == None:
+						# Initialise start time var if not done so already
+						if start_time is None:
 							start_time = time
 						time = time - start_time
-						#append lats,longs and times to the lists of each
+						# append lats,longs and times to the lists of each
 						self.lats.append(float(elem.attrib['lat']))
 						self.longs.append(float(elem.attrib['lon']))
 						self.times.append(time)
 			
-	#transform HH:MM:SS.mm into seconds
-	def get_seconds(self, time):
+	# transform HH:MM:SS.mm into seconds
+	@staticmethod
+	def get_seconds(time):
 		time = time.split("Z")[0].split("T")[1]
 		h, m, s = time.split(":")
 		return int(h) * 3600 + int(m) * 60 + float(s)
@@ -56,52 +56,54 @@ class automower_gps(object):
 	def publish_gps_file(self):
 		prev_time = None
 		time_diff = 0
-		#Iterate over each lat,long and time
+		# Iterate over each lat,long and time
 		for i in range(len(self.times)):
 			print(self.lats[i], self.longs[i], self.times[i])
-			if prev_time != None:
-				#difference in time between previous gps point and current
+			if prev_time is not None:
+				# difference in time between previous gps point and current
 				time_diff = self.times[i] - prev_time
 				print(self.times[i] - prev_time)
 				if time_diff > 0:
-					#Sleep thread for the time difference between last point and current
-					#Then publish the GPS point
+					# Sleep thread for the time difference between last point and current
+					# Then publish the GPS point
 					time.sleep(self.times[i] - prev_time)
 					self.publish_gps_point(self.lats[i], self.longs[i], self.times[i])
-			if(time_diff >= 0):
-				#Aslong as positive time_diff set new previous points time
+			if time_diff >= 0:
+				# As long as positive time_diff set new previous points time
 				prev_time = self.times[i]
 
 	def publish_gps_point(self, lat, lon, time):
-		#Publish current experiment data for mobile device to subscribe to. 
+		# Publish current experiment data for mobile device to subscribe to.
 		mat = Float32MultiArray()
-		#set up data structure
+		# set up data structure
 		mat.layout.dim.append(MultiArrayDimension())
 		mat.layout.dim[0].label = "gps_data"
 		mat.layout.dim[0].size = 3
 		mat.layout.dim[0].stride = 1
 		mat.layout.data_offset = 0
 		mat.data = [0]*3
-		#lat, lon, and time
+		# lat, lon, and time
 		mat.data[0] = lat
 		mat.data[1] = lon
 		mat.data[2] = time
-		#publish result
+		# publish result
 		self.pub_gps_data.publish(mat)
 
-	#Read gpspipe from stdin
+	# Read gpspipe from stdin
 	def read_gpspipe(self):
 		line = sys.stdin.readline()
-		if not line: print "not line"
+		if not line:
+			print("not line")
 		else:
-			#into json
+			# into json
 			sentence = json.loads(line)
-			if sentence['class'] == 'TPV':
-				#extract and publish relevant info
+			print(sentence)
+			if sentence['class'] == 'TPV' and sentence['mode'] == 3:
+				# extract and publish relevant info
 				lat = sentence['lat']
 				lon = sentence['lon']
 				time = self.get_seconds(sentence['time'])
-		 		print sentence
+				print(sentence)
 				self.publish_gps_point(lat, lon, time)
 
 	def fini(self):
@@ -109,13 +111,12 @@ class automower_gps(object):
 
 	def run(self):
 		try:
-			self.init()
-			#Live GPS
-			if(self.gps_receiver):
+			# Live GPS
+			if self.gps_receiver:
 				while not rospy.is_shutdown():
-					#function to read in GPS from gpspipe
+					# function to read in GPS from gpspipe
 					self.read_gpspipe()
-			#From file GPS
+			# From file GPS
 			else:
 				self.parse_points_file()
 				self.publish_gps_file()
@@ -124,14 +125,15 @@ class automower_gps(object):
 		finally:
 			self.fini()
 
+
 if __name__ == '__main__':
-	if(len(sys.argv) > 1):
+	if len(sys.argv) > 1:
 		arg = int(sys.argv[1]) 
-		if (arg == 0 or arg == 1):
+		if arg == 0 or arg == 1:
 			rospy.init_node('automower_gps_publisher')
-			slam_gps = automower_gps(arg)
+			slam_gps = AutomowerGpsPublisher(arg)
 			slam_gps.run()
 		else:
-			print "Error parsing command line argument", sys.argv[1]
+			print("Error parsing command line argument", sys.argv[1])
 	else:
-		print "Command line arguments required, '0' - GPS from file, '1' - Live GPS"
+		print("Command line arguments required, '0' - GPS from file, '1' - Live GPS")
