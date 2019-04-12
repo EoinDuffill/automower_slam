f490a16ff1012c5407faf5dcf72f88401a1ce90e
EoinDuffill <eoincduffill@btinternet.com>
2019-04-12 12:50:25 +0100

    Added simulated Automower position for comparison

--- src/automower_boundary_sensors.py
+++ src/automower_boundary_sensors.py
@@ -9,17 +9,18 @@ from gazebo_msgs.msg import ModelStates
 from am_driver.msg import Loop
 from std_msgs.msg import Float32, Float32MultiArray
 
-class automower_boundary_sensors(object):
 
-	def init(self):
-		#Update rate in Hz
+class AutomowerBoundarySensors(object):
+
+	def __init__(self):
+		# Update rate in Hz
 		self.update_rate = 10
-		#Setup experiment data publisher
- 	   	self.pub_odom_data = rospy.Publisher('slam/boundary_sensors', Float32MultiArray, queue_size=1)
-		#open file
+		# Setup experiment data publisher
+		self.pub_odom_data = rospy.Publisher('slam/boundary_sensors', Float32MultiArray, queue_size=1)
+		# open file
 		self.file = open("results_boundary_sensors.txt", "w")
 
-		#List of points for sensor values
+		# List of points for sensor values
 		self.x_points = []
 		self.y_points = []
 		self.sensorFC = None
@@ -30,10 +31,20 @@ class automower_boundary_sensors(object):
 		self.start_time = 0
 		self.time = 0
 
+		# SIM
+		# Get initial variables from simulator
+		data = rospy.wait_for_message("gazebo/model_states", ModelStates)
+		# get the index into the data for the automower
+		index = data.name.index("automower")
+
+		# initial x,y #SIM
+		self.x = data.pose[index].position.x
+		self.y = data.pose[index].position.y
+
 	def parse_boundary_sensors(self, data):
 
-		if(self.sensorAVG == None):
-			#Record start time, this is only called on the first iteration
+		if self.sensorAVG is None:
+			# Record start time, this is only called on the first iteration
 			self.start_time = data.header.stamp.secs + (data.header.stamp.nsecs * 10e-10)
 
 		self.sensorFC = data.frontCenter
@@ -41,16 +52,30 @@ class automower_boundary_sensors(object):
 		self.sensorRL = data.rearLeft
 		self.sensorRR = data.rearRight
 		self.sensorAVG = (self.sensorFC + self.sensorFR + self.sensorRL + self.sensorRR)/4.0
-		#Record time since start time
+		# Record time since start time
 		self.time = (data.header.stamp.secs + (data.header.stamp.nsecs * 10e-10)) - self.start_time
 
 	def write_data(self):
-		if(self.sensorAVG != None):
-			print("Time="+str(self.time)+",\tfrontCenter="+str(self.sensorFC) +"\tfrontRight="+str(self.sensorFR)+"\trearLeft="+str(self.sensorRL)+"\trearRight="+str(self.sensorRR)+"\tsensorAVG="+str(self.sensorAVG)+"\n")
-			self.file.write(str(self.time)+","+str(self.sensorFC) +"\t"+str(self.sensorFR)+"\t"+str(self.sensorRL)+"\t"+str(self.sensorRR)+"\t"+str(self.sensorAVG)+"\n")
+
+		# SIM
+		# Get initial variables from simulator
+		data = rospy.wait_for_message("gazebo/model_states", ModelStates)
+		# get the index into the data for the automower
+		index = data.name.index("automower")
+
+		# initial x,y #SIM
+		self.x = data.pose[index].position.x
+		self.y = data.pose[index].position.y
+
+		if self.sensorAVG is not None:
+			# print("Time="+str(self.time)+",\tfrontCenter="+str(self.sensorFC) +"\tfrontRight="+str(self.sensorFR)+"\trearLeft="+str(self.sensorRL)+"\trearRight="+str(self.sensorRR)+"\tsensorAVG="+str(self.sensorAVG)+"\n")
+			# self.file.write(str(self.time)+","+str(self.sensorFC) +"\t"+str(self.sensorFR)+"\t"+str(self.sensorRL)+"\t"+str(self.sensorRR)+"\t"+str(self.sensorAVG)+"\n")
+			print(str(self.x)+", "+str(self.y)+", "+str(self.sensorFC)+"\n")
+			self.file.write(str(self.y )+", "+str(self.sensorFC)+"\n")
 
 	def update(self):
 		self.write_data()
+		return
 
 	def fini(self):
 		self.file.close()
@@ -58,8 +83,7 @@ class automower_boundary_sensors(object):
 
 	def run(self):
 		try:
-			self.init()
-			#Setup subscribers
+			# Setup subscribers
 			rospy.Subscriber("loop", Loop, self.parse_boundary_sensors, queue_size=None)
 			r = rospy.Rate(self.update_rate)
 			while not rospy.is_shutdown():
@@ -73,5 +97,5 @@ class automower_boundary_sensors(object):
 
 if __name__ == '__main__':
 	rospy.init_node('automower_boundary_sensor')
-	slam_boundary_sensors = automower_boundary_sensors()
+	slam_boundary_sensors = AutomowerBoundarySensors()
 	slam_boundary_sensors.run()
